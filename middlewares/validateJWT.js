1e163f368bde102f2860d506b637b64b3d3b4da9
Hustin Jeffers <hj.paul7@gmail.com>
2021-04-05 20:57:46 -0400

    first

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -1,32 +1,41 @@
-const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
 
 const ValidateJWTMiddleware = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
-  } else if (req.headers.authorization && req.headers.authorization.includes('Bearer')) {
+  } else if (
+    req.headers.authorization &&
+    req.headers.authorization.includes("Bearer")
+  ) {
     const { authorization } = req.headers;
-    const payload = authorization ? jwt.verify(authorization.includes('Bearer') ? authorization.split(' ')[1] : authorization, process.env.JWT_SECRET): undefined;
+    const payload = authorization
+      ? jwt.verify(
+          authorization.includes("Bearer")
+            ? authorization.split(" ")[1]
+            : authorization,
+          process.env.JWT_SECRET
+        )
+      : undefined;
     if (payload) {
       User.findOne({
         where: {
-          id: payload.id
-        }
-      })
-      .then(user => {
+          id: payload.id,
+        },
+      }).then((user) => {
         req.user = user;
         next();
-      })
+      });
     } else {
       res.status(401).json({
-        message: 'Not allowed'
+        message: "Not allowed",
       });
     }
   } else {
     res.status(401).json({
-      message: 'Not allowed'
+      message: "Not allowed",
     });
   }
-}
+};
 
 module.exports = ValidateJWTMiddleware;
